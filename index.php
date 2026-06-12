f74fbe0c8d314f29d3e59282be9c8af6ca5ef5cd
tranmongbinh2005-commits <tranmongbinh2005@gmail.com>
2026-06-12 10:56:03 +0700

    api

--- index.php
+++ index.php
@@ -15,6 +15,56 @@ if ($url === '') {
 
 $segments = explode('/', $url);
 
+// Định tuyến các yêu cầu API
+if (strtolower($segments[0] ?? '') === 'api' && isset($segments[1])) {
+    $apiControllerName = ucfirst($segments[1]) . 'ApiController';
+    if (file_exists('app/controllers/' . $apiControllerName . '.php')) {
+        require_once 'app/controllers/' . $apiControllerName . '.php';
+        $controller = new $apiControllerName();
+        $method = $_SERVER['REQUEST_METHOD'];
+        $id = $segments[2] ?? null;
+
+        switch ($method) {
+            case 'GET':
+                $action = $id ? 'show' : 'index';
+                break;
+            case 'POST':
+                $action = 'store';
+                break;
+            case 'PUT':
+                if ($id) {
+                    $action = 'update';
+                }
+                break;
+            case 'DELETE':
+                if ($id) {
+                    $action = 'destroy';
+                }
+                break;
+            default:
+                http_response_code(405);
+                echo json_encode(['message' => 'Method Not Allowed']);
+                exit;
+        }
+
+        if (method_exists($controller, $action)) {
+            if ($id) {
+                call_user_func_array([$controller, $action], [$id]);
+            } else {
+                call_user_func_array([$controller, $action], []);
+            }
+        } else {
+            http_response_code(404);
+            echo json_encode(['message' => 'Action not found']);
+        }
+        exit;
+    } else {
+        http_response_code(404);
+        echo json_encode(['message' => 'Controller not found']);
+        exit;
+    }
+}
+
 // Kiểm tra phần đầu tiên của URL để xác định controller
 $controllerName = isset($segments[0]) && $segments[0] != '' ? ucfirst($segments[0]) . 'Controller' : 'DefaultController';
 // Kiểm tra phần thứ hai của URL để xác định action
